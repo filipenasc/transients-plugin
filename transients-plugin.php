02b3781d7a7ea9136ee1597fe27a0aca91c80137
Filipe Nascimento <filipemateusn@gmail.com>
2016-12-01 17:58:18 -0200

    initial commit

---
+++ transients-plugin.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+Plugin Name: 	Transients Plugin
+Description:  Testing deployment of WordPress Plugins with Transient
+Version:      0.0.1
+Author:       Filipe Nascimento
+Author URI:   http://resultadosdigitais.com.br
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Integração RD Station is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+Integração RD Station is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with Integração RD Station. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+*/
